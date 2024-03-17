7d4919436671cf77cc5e74dfeaed1c8fb5e3849d
Manomoy Biswas <manomoy.biswas@bitcanny.com>
2024-03-17 20:58:44 +0530

    new line separator handled

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# class to calculate sum of digits in a string
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
@@ -5,6 +8,6 @@ class StringCalculator
     # keeping this conditional return to avoid split, map and sum operation for single digit string
     return numbers.to_i if numbers.length == 1
 
-    numbers.split(',').map(&:to_i).sum
+    numbers.gsub('\n', ',').split(',').map(&:to_i).sum
   end
 end
