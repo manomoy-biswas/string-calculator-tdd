0202cbb7c921cd7bfc4b203c6b1f2a2a33706b6c
Manomoy Biswas <manomoy.biswas@bitcanny.com>
2024-03-17 20:28:32 +0530

    unknown length of numbers are handled

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,9 +2,9 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
+    # keeping this conditional return to avoid split, map and sum operation for single digit string
     return numbers.to_i if numbers.length == 1
 
-    numbers = numbers.split(',').map(&:to_i)
-    numbers[0] + numbers[1]
+    numbers.split(',').map(&:to_i).sum
   end
 end
