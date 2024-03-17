3d5c9785df603f699215b94fe02fa833e1c77560
Manomoy Biswas <manomoy.biswas@bitcanny.com>
2024-03-17 20:23:02 +0530

    code added to add 2 digit in a string separated by `,`(comma)

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,5 +3,8 @@ class StringCalculator
     return 0 if numbers.empty?
 
     return numbers.to_i if numbers.length == 1
+
+    numbers = numbers.split(',').map(&:to_i)
+    numbers[0] + numbers[1]
   end
 end
